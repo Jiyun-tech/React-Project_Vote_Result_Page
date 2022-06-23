eb3ef98a48125e39a655d310407c1d0f5751275f
Jiyun-tech <100850714+Jiyun-tech@users.noreply.github.com>
2022-06-23 17:08:10 +0900

    React - result bar width revised

--- src/Results.js
+++ src/Results.js
@@ -1,26 +1,25 @@
 import './Results.css';
 import redbar from './redbar.png'
 
-function Result(props){
+function Result(props) {
     return (
-    <table className="do_vote">
-        <tbody>
-        <tr> 
-            <td className="printname">
-               <a 
-                className="App-link"
-                href="https://reactjs.org"
-                > 
-                {props.id}번 {props.name}
-               </a>
-            </td>
-            <td className="each_result_bar">
-                <img src={redbar} height="20" width={props.voteRate*7} />
-                {props.voteEarned} ({props.voteRate}%)
-            </td>
-        </tr>
-        </tbody>
-    </table>
+        <table className="do_vote">
+            <tbody>
+                <tr>
+                    <td className="printname">
+                        <a
+                            href="./ResultDetails.js"
+                        >
+                            {props.id}번 {props.name}
+                        </a>
+                    </td>
+                    <td className="each_result_bar">
+                        <img src={redbar} height="20" width={`${props.voteRate*0.8}%`} />
+                        {props.voteEarned} ({props.voteRate}%)
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     )
 }
 export default Result
